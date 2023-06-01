08302696e9a756a09471a112d2b9304752ab643a
Vina <vinafujiyanti22@gmail.com>
2023-06-01 17:30:09 +0700

    first commit new update

--- application/controllers/api/Login.php
+++ application/controllers/api/Login.php
@@ -3,11 +3,15 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . '/libraries/JWT.php';
 
 use Restserver\Libraries\REST_Controller;
+use \Firebase\JWT\JWT;
 
 class Login extends REST_Controller
 {
+    private $secretkey = 'secretloginkey1234554321';
+
     public function index_post()
     {
         $this->load->library('form_validation');
@@ -15,6 +19,8 @@ class Login extends REST_Controller
 
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required|trim');
+
+        $date = new DateTime();
 
         if ($this->form_validation->run() == FALSE) 
         {
@@ -24,6 +30,7 @@ class Login extends REST_Controller
                 'message' => preg_replace('/[^A-Za-z0-9_~`\/@!$.%^#&*\\()+-=]/',' ', validation_errors()),
             ], 400);
         } 
+
         else {
             $email    = $this->input->post('email');
             $password = $this->input->post('password');
@@ -45,9 +52,21 @@ class Login extends REST_Controller
             }
 
             else {
+                $payload = array (
+                    "uid"   => $users['id_user'],
+                    "iat"   => $date->getTimeStamp(),
+                );
+                
+                $token  = JWT::encode($payload, $this->secretkey, 'HS256');
+
                 $this->response([
                     'error'   => false,
                     'message' => 'Successful login!',
+                    'result'  => array (
+                        'id_user'   => $users['id_user'],
+                        'full_name' => $users['full_name'],
+                        'token'     => $token, 
+                    )
                 ], 200);
             }
         }
--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -6,5 +6,5 @@ class Users_model extends CI_Model {
     public function add_users($data) {
         $this->db->insert('users', $data);
         return $this->db->insert_id();
-    }  
+    }
 }
