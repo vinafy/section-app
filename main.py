4f3f2b54c0df70250048740e2bf89a758533b046
unknown <rohidalma@gmail.com>
2023-06-14 20:34:38 +0700

    update ml model

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
-from news_recommendation_2 import predict_article
+from news_recommendation import predict_article
 
 app = Flask(__name__)
 
 @app.route('/api/<user_id>', methods=['GET'])
-def hello(user_id):
+def article_api(user_id):
     data = predict_article(user_id,10)
     return {
-        'massage': 'top 10 recommendations article',
+        'massage': 'top 10 recommendations article for user {}'.format(user_id),
         'title': data
     } 
 
